ad4ea776bd5775901040162891aad11f1357834c
Alexsander Gvozdev <a.gvozdev@outlook.com>
2018-05-04 00:35:31 +0300

    Update populateDB.sql

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -1,1 +1,1 @@
-INSERT INTO customers VALUES (1, 'Ivan', 'Ivanov', 'Ukraine, Kiev, Vesennyaya, 1', 10000.00);
+INSERT INTO customers VALUES (1, 'Alexsander', 'Gvozdev', 'Belarus, Minsk, Kalinovskogo st, 31', 10000.00);
